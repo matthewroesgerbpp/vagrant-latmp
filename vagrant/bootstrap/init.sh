2fdd6e730408d9b9e3889395ddea8ea5d878f976
Micky Hulse <mickyhulse@gmail.com>
2018-07-29 00:42:30 -0700

    SAVEPOINT

--- vagrant/bootstrap/init.sh
+++ vagrant/bootstrap/init.sh
@@ -9,18 +9,17 @@ export GIT_CONFIG_NAME=''
 export GIT_CONFIG_EMAIL=''
 export RUBY_VERSION='2.3.1' # rvm list known
 
-# Finished:
-# source /vagrant/bootstrap/functions.sh
-# source /vagrant/bootstrap/firewall.sh
-# source /vagrant/bootstrap/nano.sh
-# source /vagrant/bootstrap/git.sh
+source /vagrant/bootstrap/functions.sh
+source /vagrant/bootstrap/firewall.sh
+source /vagrant/bootstrap/nano.sh
+source /vagrant/bootstrap/git.sh
 source /vagrant/bootstrap/mysql.sh
-# source /vagrant/bootstrap/http.sh
-# source /vagrant/bootstrap/tomcat.sh
-# source /vagrant/bootstrap/node.sh
-# source /vagrant/bootstrap/php.sh
-# source /vagrant/bootstrap/phpmyadmin.sh
-# source /vagrant/bootstrap/composer.sh
-# source /vagrant/bootstrap/ruby.sh
-# source /vagrant/bootstrap/mailcatcher.sh
-# source /vagrant/bootstrap/profile.sh
+source /vagrant/bootstrap/http.sh
+source /vagrant/bootstrap/tomcat.sh
+source /vagrant/bootstrap/node.sh
+source /vagrant/bootstrap/php.sh
+source /vagrant/bootstrap/phpmyadmin.sh
+source /vagrant/bootstrap/composer.sh
+source /vagrant/bootstrap/ruby.sh
+source /vagrant/bootstrap/mailcatcher.sh
+source /vagrant/bootstrap/profile.sh
