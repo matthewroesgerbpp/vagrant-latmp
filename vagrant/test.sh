4ddb2008bf134c07cd8299597150ac798d83975f
Micky Hulse <mickyhulse@gmail.com>
2018-07-29 23:57:48 -0700

    SAVEPOINT

--- vagrant/test.sh
+++ vagrant/test.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
+
+# To see what packages `groupinstall` installs, run:
+# $ yum groups info development
+sudo yum --assumeyes groups install
+sudo yum --assumeyes groupinstall development
+
+# Additional packages needed:
+sudo yum --assumeyes install \
+  bzip2-devel \
+  curl \
+  gnupg2 \
+  libyaml-devel \
+  openssl-devel \
+  readline-devel \
+  sqlite-devel \
+  zlib-devel
+
+bundle install \
+--path /var/ruby/test/ \
+--gemfile /var/ruby/test/Gemfile
